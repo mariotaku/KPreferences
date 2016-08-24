feb98f049b341976621d7789d48e7ed044ddb01a
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-08-24 17:31:18 +0800

    updated README

---
+++ README.md
@@ -0,0 +1,67 @@
+# KPreferences
+
+Better SharedPreferences wrapper
+
+---
+
+[![](https://jitpack.io/v/mariotaku/KPreferences.svg)](https://jitpack.io/#mariotaku/KPreferences)
+
+KPreferences is a Kotlin wrapper for Android SharedPreferences, to make it simpler and safer (avoiding null problems at compile time thanks to Kotlin)
+
+## Features
+
+### Get value
+
+````
+val stringPrefs = KStringKey("name", "def")
+val str = prefs[stringPrefs] // This is a string
+val intPrefs = KIntKey("name", 1)
+val int = prefs[stringPrefs] // This is an integer
+````
+
+### Set value
+
+````
+val stringPrefs = KStringKey("name", "def")
+prefs[stringPrefs] = "foo" // Calls SharedPreferences.Editor#apply() immediately
+prefs.edit {
+    this[stringPrefs] = "bar"
+} // Calls SharedPreferences.Editor#apply() after block executed
+````
+
+### Check existence
+
+````
+val stringPrefs = KStringKey("name", "def")
+if (stringPrefs in prefs) {
+    // Check if SharedPreferences contains this key
+}
+if ("keyString" in prefs) {
+    // Check if SharedPreferences contains this key
+}
+````
+
+### Nullable value
+
+````
+val nullableStringPrefs = KNullableStringKey("name", null)
+val str = prefs[nullableStringPrefs] // This is a nullable string
+````
+
+### Custom type
+
+````
+class KDateKey(key: String, def: Date) : KSimpleKey<Date>(key, def) {
+    override fun write(editor: SharedPreferences.Editor, value: Date) {
+        editor.putLong(key, value.time)
+    }
+
+    override fun read(preferences: SharedPreferences): Date {
+        return Date(preferences.getLong(key, def.time))
+    }
+
+}
+
+val datePrefs = KDateKey("name", Date())
+val date = prefs[datePrefs] // This is a date object
+````
--- app/src/main/java/org/mariotaku/kpreferences/sample/MainActivity.kt
+++ app/src/main/java/org/mariotaku/kpreferences/sample/MainActivity.kt
@@ -1,27 +1,69 @@
 package org.mariotaku.kpreferences.sample
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.mariotaku.kpreferences.KIntKey
-import org.mariotaku.kpreferences.KPreferences
-import org.mariotaku.kpreferences.KStringKey
+import org.mariotaku.kpreferences.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val prefs: KPreferences by lazy { KPreferences.get(this, "test", Context.MODE_PRIVATE) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val prefs = KPreferences.get(this, "test", Context.MODE_PRIVATE)
+        getValue()
+        setValue()
+        checkExistence()
+        nullableValue()
+        customType()
+    }
+
+    fun getValue() {
         val stringPrefs = KStringKey("name", "def")
+        val str = prefs[stringPrefs] // This is a string
         val intPrefs = KIntKey("name", 1)
+        val int = prefs[stringPrefs] // This is an integer
+    }
+
+    fun setValue() {
+        val stringPrefs = KStringKey("name", "def")
+        prefs[stringPrefs] = "foo" // Calls SharedPreferences.Editor#apply() immediately
         prefs.edit {
-            this[stringPrefs] = "foo"
-            this[intPrefs] = 2
-        }
-        val str = prefs[stringPrefs]
+            this[stringPrefs] = "bar"
+        } // Calls SharedPreferences.Editor#apply() after block executed
+    }
+
+    fun checkExistence() {
+        val stringPrefs = KStringKey("name", "def")
         if (stringPrefs in prefs) {
+            // Check if SharedPreferences contains this key
+        }
+        if ("keyString" in prefs) {
+            // Check if SharedPreferences contains this key
+        }
+    }
+
+    fun nullableValue() {
+        val nullableStringPrefs = KNullableStringKey("name", null)
+        val str = prefs[nullableStringPrefs] // This is a nullable string
+    }
+
+    fun customType() {
+        class KDateKey(key: String, def: Date) : KSimpleKey<Date>(key, def) {
+            override fun write(editor: SharedPreferences.Editor, value: Date) {
+                editor.putLong(key, value.time)
+            }
+
+            override fun read(preferences: SharedPreferences): Date {
+                return Date(preferences.getLong(key, def.time))
+            }
 
         }
+
+        val datePrefs = KDateKey("name", Date())
+        val date = prefs[datePrefs] // This is a date object
     }
 }
